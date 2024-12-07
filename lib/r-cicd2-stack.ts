58620faf7614fcac4cdf8a0f7d4ac6a673df59a1
Ramanpreet Kaur <ramanpreetk197@gmail.com>
2024-12-06 19:47:11 -0500

    Initial commit with project files

--- lib/r-cicd2-stack.ts
+++ lib/r-cicd2-stack.ts
@@ -1,16 +1,41 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 
 export class RCicd2Stack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+  
+    const bucket = new s3.Bucket(this, 'S3Bucket', {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'RCicd2Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+  
+    const lambdaFn = new lambda.Function(this, 'Lambda', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromInline(`
+        exports.handler = async (event) => {
+          console.log('Event:', event);
+          return { statusCode: 200, body: 'Hello' };
+        };
+      `),
+      environment: {
+        BUCKET_NAME: bucket.bucketName,
+      },
+    });
+
+
+    const table = new dynamodb.Table(this, 'DynamoDBT', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    
+    bucket.grantReadWrite(lambdaFn);
   }
 }
+
